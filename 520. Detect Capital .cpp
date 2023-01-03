fec50d10b60d570f5c5b708c2093e5200e9bbc9b
Abdelrahman Helmy <104210345+Helmy-JR@users.noreply.github.com>
2023-01-04 00:15:16 +0200

    Create 520. Detect Capital .cpp

---
+++ 520. Detect Capital .cpp
@@ -0,0 +1,15 @@
+class Solution { //Author: Abdelrahman Helmy
+public:
+    bool detectCapitalUse(string s) {
+        bool chk=isupper(s.front());
+        int cnt=0;
+        for(auto& i : s){
+            if(i>='A'&&i<='Z') cnt++;
+        }
+        if(cnt>0){
+            if(cnt==s.size() || (cnt==1 && chk==true)) return true;
+            else return false;
+        }
+        return true;
+    }
+};
