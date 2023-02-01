ddab0efb98a12093c00eaea31ab2f58b9b11fb7f
Abdelrahman Helmy <104210345+Helmy-JR@users.noreply.github.com>
2023-02-01 21:40:28 +0200

    Create 31 - N-th Tribonacci Number .cpp

---
+++ January/31 - N-th Tribonacci Number .cpp
@@ -0,0 +1,13 @@
+// Author: Abdelrahman Helmy
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector < int > ans(n + 5);
+        ans[0] = 0, ans[1] = 1, ans[2] = 1;
+        for(int i = 3; i <= n; i++)
+            ans[i] = ans[i - 1] + ans[i - 2] + ans[i - 3];
+
+        return ans[n];
+    }
+};
